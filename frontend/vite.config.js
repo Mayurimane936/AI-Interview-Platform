70933cd0f3f9bb0724dc8418a084cc526251b903
Mayurimane936 <mayurimane936@gmail.com>
2026-09-19 16:28:28 +0530

    minor fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  envPrefix: [
+    'VITE_',
+    'API_',
+  ],
   plugins: [
     react(),
     tailwindcss(),
